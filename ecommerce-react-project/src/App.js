4b4825237aab568f4749d3dd2028d90062d351b7
salomon.escobar <salomon.escobar@company-corp.com>
2020-04-09 12:37:24 -0500

    ecommerce-react-project | modified app.js to take in form input styles/components and sign in/sign up from comp. and files

--- ecommerce-react-project/src/App.js
+++ ecommerce-react-project/src/App.js
@@ -3,7 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
